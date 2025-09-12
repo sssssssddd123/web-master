985a38b8db1e745b606dd07919cd259ef294bfd5
song <islbvv@gmail.com>
2025-09-12 17:20:38 +0900

    9월 12일

---
+++ public/js/2nd/function3.js
@@ -0,0 +1,5 @@
+// function3.js
+
+let num0 = Math.max(10, 20);
+
+console.log(`큰 값: ${num0}`);
---
+++ public/js/2nd/todo.js
@@ -0,0 +1,11 @@
+// todo.js
+
+// 2개의 값을 입력받도록 화면을 html에 구성
+// 계산하는 기능을 구현
+// +, -, *, /
+// plus, minus, multiply, divide 함수 이름
+
+
+
+
+
