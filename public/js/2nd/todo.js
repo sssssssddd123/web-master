fcedbb23d10c67b00afd13860d76e358291eb099
song <islbvv@gmail.com>
2025-09-14 20:14:26 +0900

    계산기 test

--- public/js/2nd/todo.js
+++ public/js/2nd/todo.js
@@ -5,7 +5,26 @@
 // +, -, *, /
 // plus, minus, multiply, divide 함수 이름
 
+function calculate() {
+  let num0 = parseInt(document.querySelector('#num0').value),
+      num1 = parseInt(document.querySelector('#num1').value),
+      op = document.querySelector('#selector').value,
+      result = 0;
 
+  switch (op) {
+    case '+':
+      result = num0+num1;
+      break;
+    case '-':
+      result = num0-num1;
+      break;
+    case '*':
+      result = num0*num1;
+      break;
+    case '/':
+      result = num0/num1;
+  }
 
+  document.querySelector('#num2').value = result;
 
-
+}
