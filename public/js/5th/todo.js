32d8f3e7750d804d00efd8a154588b4277407de7
song <islbvv@gmail.com>
2025-09-18 00:07:28 +0900

    for todo

--- public/js/5th/todo.js
+++ public/js/5th/todo.js
@@ -2,16 +2,70 @@
 
 // 반복문을 활용해서 출력
 
-// *
-// **
-// ***
-// ****
+//     *
+//    **
+//   ***
+//  ****
 // *****
 
-let s = '';
-  console.log(s)
+let star = "";
+let spacebar = "";
+let num = 0;
 
 for (let i = 0; i < 5; i++) {
-  s += '*';
-  console.log(s)
+  for (let i = 0; i < 4 - num; i++) {
+    spacebar += " ";
+  }
+
+  num++
+  star += "*";
+  console.log(spacebar + star);
+
+  spacebar = "";
+}
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+star = "";
+spacebar = "";
+num = 0;
+
+for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 4 - num; i++) {
+    spacebar += " ";
+  }
+
+  num++
+  star += "*";
+  console.log(spacebar + star);
+  star += "*";
+
+  spacebar = "";
+}
+
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+star = "";
+spacebar = "";
+num = 7;
+
+
+for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < num; i++) {
+    star += "*";
+  }
+
+  spacebar += " ";
+  console.log(spacebar + star);
+
+  star = "";
+  num -= 2;
 }
